e9a57245ccf627c6faac8d980de93733f8ab9d56
Awaysed <122900171@qq.com>
2024-11-23 22:24:16 +0800

    add:vant

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,16 @@
 const { defineConfig } = require("@vue/cli-service");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const { VantResolver } = require("@vant/auto-import-resolver");
+// const AutoImport = require("unplugin-auto-import/webpack");
+const Components1 = require("unplugin-vue-components/webpack");
+console.log(1111,Components1);
 module.exports = defineConfig({
   transpileDependencies: true,
-  configureWebpack:{
-    plugins: [new NodePolyfillPlugin()],
-  }
+  configureWebpack: {
+    plugins: [
+      new NodePolyfillPlugin(),
+      // AutoImport({ resolvers: [VantResolver()] }),
+      Components1.default({ resolvers: [VantResolver()] }),
+    ],
+  },
 });
