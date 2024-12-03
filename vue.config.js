2905190454e4430d8d12afdf3a06d42d6f451480
Awaysed <122900171@qq.com>
2024-12-03 22:20:39 +0800

    fix:解决process 问题

--- vue.config.js
+++ vue.config.js
@@ -3,15 +3,22 @@ const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 const { VantResolver } = require("@vant/auto-import-resolver");
 // const AutoImport = require("unplugin-auto-import/webpack");
 const Components1 = require("unplugin-vue-components/webpack");
-console.log(1111,Components1);
+console.log(1111, NodePolyfillPlugin);
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   configureWebpack: {
     plugins: [
-      new NodePolyfillPlugin(),
+      new NodePolyfillPlugin({
+        additionalAliases: ['process', 'punycode']
+      }), // 启用调试日志
       // AutoImport({ resolvers: [VantResolver()] }),
       Components1.default({ resolvers: [VantResolver()] }),
     ],
+    // resolve: {
+    //   alias: {
+    //     process: "process/browser",
+    //   },
+    // },
   },
 });
