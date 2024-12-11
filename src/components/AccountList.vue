fe3144974a6acf132b4e0736546740aea4651482
Awaysed <122900171@qq.com>
2024-12-11 10:58:22 +0800

    add:account

--- src/components/AccountList.vue
+++ src/components/AccountList.vue
@@ -6,17 +6,32 @@
         v-for="item in walletInfo"
         icon="location-o"
         :title="item.address"
-        value="内容"
+        :value="item.balance"
       />
     </van-cell-group>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import Web3 from "web3";
 import store2 from "store2";
 const walletInfo = ref([]);
 walletInfo.value = store2.get("walletInfo") || [];
+const web3 = new Web3(
+  Web3.givenProvider ||
+    "wss://sepolia.infura.io/ws/v3/9592f660765f4dd19d74b5248ba9b66c"
+);
+const walletInfoC = computed(() => {
+  return walletInfo.value.map(async (item) => {
+    web3.eth.getBalance(item.address).then((res) => {
+      item.balance = web3.utils.fromWei(res, "ether");
+    });
+    const res = await web3.eth.getBalance(item.address);
+    item.balance = web3.utils.fromWei(res, "ether");
+    return item;
+  });
+});
 </script>
 
 <style scoped lang="less">
