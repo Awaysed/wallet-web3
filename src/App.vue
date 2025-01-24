ef17a961b1e63d6b74b7aea2f4509e0f7f37aebe
Awaysed <122900171@qq.com>
2025-01-24 17:26:36 +0800

    add:web3

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <h2>HelloWorld</h2>
-  <ButtonVue />
-  <AccountList/>
+  <!-- <ButtonVue />
+  <AccountList/> -->
 </template>
 
 <script setup>
-import ButtonVue from "./components/Button1.vue";
-import AccountList from "./components/AccountList.vue";
+// import ButtonVue from "./components/Button1.vue";
+// import AccountList from "./components/AccountList.vue";
+import Web3 from 'web3'
+import mtcContract from "./contract/MyToken.json";
+console.log(222,mtcContract);
+const geerLiWs = "wss://sepolia.infura.io/ws/v3/9592f660765f4dd19d74b5248ba9b66c"
+const web3 = new Web3(
+  Web3.givenProvider ||
+  geerLiWs
+);
 </script>
 
 <style></style>
